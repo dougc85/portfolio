0667a7b6f6c84be0ab80174fba4aadb4af8d1614
Doug Carter <dougc85@gmail.com>
2023-02-01 12:07:36 -0800

    reordered skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -12,14 +12,15 @@ function Skills() {
         <i className="devicon-react-original-wordmark colored"></i>
         {/* <i className="devicon-typescript-plain colored"></i>
         <i className="devicon-jest-plain colored"></i> */}
-        <i className="devicon-sass-original colored"></i>
-        <img src="./images/styled-components.png" alt="styled-components icon" />
-        <i className="devicon-firebase-plain-wordmark colored"></i>
-        <i className="devicon-git-plain colored"></i>
-        {/* <i className="devicon-webpack-plain-wordmark colored"></i> */}
         <i className="devicon-nodejs-plain-wordmark colored"></i>
         <i className="devicon-express-original-wordmark"></i>
         <i className="devicon-mongodb-plain-wordmark colored"></i>
+        <i className="devicon-firebase-plain-wordmark colored"></i>
+        <i className="devicon-sass-original colored"></i>
+        <img src="./images/styled-components.png" alt="styled-components icon" />
+        <i className="devicon-git-plain colored"></i>
+        {/* <i className="devicon-webpack-plain-wordmark colored"></i> */}
+
         <i className="devicon-python-plain-wordmark colored"></i>
       </div>
     </SkillsStyled>
